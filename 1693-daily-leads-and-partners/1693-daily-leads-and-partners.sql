19f53595e64dc36f1c0006d86c7813ae0b7e8d7b
Shaily781 <76932285+Shaily781@users.noreply.github.com>
2025-10-10 16:37:16 +0530

    Time: 418 ms (92.31%), Space: 0B (100%) - LeetHub

---
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+Select 
+    date_id, 
+    make_name, 
+    count(distinct lead_id) unique_leads, 
+    count(distinct partner_id) unique_partners
+From DailySales
+group by date_id, make_name
