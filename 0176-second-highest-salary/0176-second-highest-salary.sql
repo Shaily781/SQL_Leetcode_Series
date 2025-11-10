84243f70892e1aca0f6a08ab5de67ba719def2d7
Shaily Soni <76932285+Shaily781@users.noreply.github.com>
2025-11-10 20:26:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,40 @@
 /* Write your T-SQL query statement below */
+
+With SalaryRank As (
+    Select salary,
+    dense_rank() over (order by salary desc) as rnk
+    from Employee
+)
+Select isnull((Select salary from SalaryRank where rnk = 2), NULL) SecondHighestSalary 
+
+
+
+
+
+
+
+
+
+
+
+/*
+Select isnull(
+    (Select Distinct salary
+    From Employee
+    Order by salary Desc
+    Offset 1 rows fetch next 1 rows only),NULL
+) SecondHighestSalary 
+*/
+
+
+
+
+
+/*
 Select isnull(
     (Select Distinct Top 1 salary
     From Employee
     where salary < (Select max(salary) from Employee)
     Order by salary Desc),NULL
 ) SecondHighestSalary 
+*/
